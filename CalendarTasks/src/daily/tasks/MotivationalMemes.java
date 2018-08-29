1219ef69902c3c29043dd1ea57f983f230620224
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-08-28 21:14:00 -0400

    added generateNatureNews() method

--- CalendarTasks/src/daily/tasks/MotivationalMemes.java
+++ CalendarTasks/src/daily/tasks/MotivationalMemes.java
@@ -3,11 +3,12 @@ package daily.tasks;
 import org.jsoup.Jsoup;  
 import org.jsoup.nodes.Document;  
 import org.jsoup.nodes.Element;
-import org.jsoup.select.*;
+import org.jsoup.select.Elements;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Random;
+import java.io.IOException;
 import java.net.URL;
 
 public class MotivationalMemes {
@@ -16,13 +17,12 @@ public class MotivationalMemes {
 
     //browser user agent to reduce 403 errors
     public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
-
-    public static void main(String[] args) throws Exception {
+        
+    public static URL generateMemes() throws IOException{
     	
-    }
-    
-    public static URL generateMemes() throws Exception{
-    	
+    	Pattern pat = Pattern.compile("http.*?.(?:jpg)|http.*?.(?:jpeg)|http.*?.(?:gif)|http.*?.(?:png)|http.*?.(?:tif)");
+        ArrayList<String> resultUrls = new ArrayList<String>();
+        
     	//Terms to be randomly selected for search query
     	ArrayList<String> term1 = new ArrayList<String>();
     	term1.add("eloquent"); term1.add("emotional");	term1.add("expressive"); term1.add("gripping");	term1.add("heartbreaking");	term1.add("heartrending"); term1.add("inspirational"); term1.add("inspiring");
@@ -36,11 +36,10 @@ public class MotivationalMemes {
         		.referrer("www.google.com").userAgent(USER_AGENT).get();
         
         /*Grabs the elements of the div with class rg_meta as this is where the links to the images are located
-         * crappy regex pattern that gets the job done to extract the URL from each element and stores it in an arraylist.
+         * regex pattern that gets the job done to extract the URL from each element and stores it in an arrayList.
          */
         Elements elements = doc.select("div.rg_meta");
-        Pattern pat = Pattern.compile("http.*?.(?:jpg)|http.*?.(?:jpeg)|http.*?.(?:gif)|http.*?.(?:png)|http.*?.(?:tif)");
-        ArrayList<String> resultUrls = new ArrayList<String>();
+        
             
         for (Element element : elements) {
             if (element.childNodeSize() > 0) {
@@ -50,16 +49,47 @@ public class MotivationalMemes {
                 	resultUrls.add(matcher.group(0));
                 }
             }
-        }        
+        }
         
-        // randomly selects an index of the result arraylist and then returns it
+        // randomly selects an index of the result arrayList and then returns it
         URL urlInput = new URL(resultUrls.get(getRandomInts(0,resultUrls.size()-1)));
         
         return urlInput;
-    	} 
+    } 
+        
+    public static String generateNatureNews() throws IOException{
+        	
+      	
+        String a;
+        ArrayList<String> resultUrls = new ArrayList<String>();
+        String bodyText;
+        //Fetch the page
+        Document doc = Jsoup.connect("https://www.nature.com/nature/articles?type=news").userAgent(USER_AGENT).get();
+            
+        /*
+        * Grabs the h3 header class mb10 and the anchor following it
+        */
+        Elements elements = doc.select("h3.mb10 > a");
+        
+        //grabs the href attr from the anchor, appends to the parent website and adds to the arrayList
+        for (Element element : elements) {
+            if (element.childNodeSize() > 0) {
+                a = element.attr("href");
+                a = "https://www.nature.com" + a;
+                resultUrls.add(a);
+            }
+        }   
+        
+        // randomly selects an index of the result arrayList and then returns it
+        
+        doc = Jsoup.connect(resultUrls.get(getRandomInts(0,resultUrls.size()-1))).userAgent(USER_AGENT).get();
+        bodyText = doc.select("div.article__body > p").toString();
+        
+        return bodyText;
+    } 
     	
     
-    //generates random numbers for our arraylists
+    //generates random numbers for our arrayLists
     public static int getRandomInts(int min, int max){
     	Random random = new Random();
     	return random.ints(min,(max+1)).findFirst().getAsInt();
